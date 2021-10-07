7f45875aa983d951ba22fc1fe4dc934b61d380a3
Sachin <sachinkshuklaoo7@gmail.com>
2021-10-07 14:04:53 +0530

    Add heroku.yml

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 //Load express module with `require` directive
 var express = require('express')
+const dotenv = require('dotenv');
+
 var app = express()
+dotenv.config({ path: `/.env.development` })
 
 //Define request response in root URL (/)
 app.get('/', function (req, res) {
     res.send('Hello World!')
 })
 
+const PORT = process.env.PORT || 8000;
+
+console.log(PORT)
+
 //Launch listening server on port 8080
-app.listen(8080, function () {
+app.listen(PORT, function () {
     console.log('app listening on port 8080!')
 })
